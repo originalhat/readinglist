1f4b09e31b982b7f7559ea7f1e554bb610a85bc6
Devin Brown <devin.g.brown@gmail.com>
2017-11-25 08:37:21 -0700

    Fucking templates.

--- src/main/java/com/example/readinglist/ReadingListApplication.java
+++ src/main/java/com/example/readinglist/ReadingListApplication.java
@@ -3,11 +3,12 @@ package com.example.readinglist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// enable component-scanning and auto-configuration
 @SpringBootApplication
 public class ReadingListApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReadingListApplication.class, args);
-
-	}
+public static void main(String[] args) {
+    // bootstrap application
+    SpringApplication.run(ReadingListApplication.class, args);
+  }
 }
