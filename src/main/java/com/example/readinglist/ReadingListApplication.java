596df94f9a8d6cc1239117db81de18a38565736d
Devin Brown <devin.g.brown@gmail.com>
2017-11-25 08:25:30 -0700

    A fix I believe, not like I tested or anything

--- src/main/java/com/example/readinglist/ReadingListApplication.java
+++ src/main/java/com/example/readinglist/ReadingListApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReadingListApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ReadingListApplication.class, args);
+
 	}
 }
