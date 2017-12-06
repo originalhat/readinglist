0b84b8f95fa2addb022caf0c036de7b17f735ea8
Devin Brown <devin.g.brown@gmail.com>
2017-12-05 19:57:44 -0700

    done. going to bed now.

---
+++ src/main/kotlin/com/originalhat/readinglist/ReadinglistApplication.kt
@@ -0,0 +1,11 @@
+package com.originalhat.readinglist
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ReadinglistApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ReadinglistApplication::class.java, *args)
+}
